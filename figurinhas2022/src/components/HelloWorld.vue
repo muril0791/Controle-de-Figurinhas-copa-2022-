b0b675d2813c82cc92d91427afe56c0248aef461
Murilo Ferreira <murilosantos.ms791@gmail.com>
2022-09-16 23:55:14 +0000

    Start Project cup 2022

---
+++ figurinhas2022/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app id="inspire">
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col v-for="n in 24" :key="n" cols="3">
+            <v-card height="300" width="250"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  //
+};
+</script>
